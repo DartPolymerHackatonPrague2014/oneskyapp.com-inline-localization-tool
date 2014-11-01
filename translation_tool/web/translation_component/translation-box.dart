1cc1af97ef67bef57ac16d36d2ddaddddd8a4dc5
Vlastimil Dolejš <vlastimil.dolejs@diribet.cz>
2014-11-01 15:27:49 +0100

    translation component can select language and accepts existing translations

--- translation_tool/web/translation_component/translation-box.dart
+++ translation_tool/web/translation_component/translation-box.dart
@@ -1,16 +1,35 @@
 import "dart:html";
+import 'dart:convert';
 import "package:polymer/polymer.dart";
 
 @CustomTag('translation-box')
 class TranslationBoxElement extends PolymerElement {
-  @published String messsage_key = null;
-  @published String default_lang = null;
-  @observable String translation = "initial translation";
+  @published String messsage_key;
+  @published String default_lang;
+  @observable String translation;
+  @published String translations;
+  
+  @observable List languages;
+  @observable String selectedLanguage;
+  Map translationsMap;
   
   TranslationBoxElement.created() : super.created() {
+    translationsMap = JSON.decode(translations);
+    
+    languages = new List.from(translationsMap.keys);
+    selectedLanguage = default_lang;
   }
   
   void submitTranslation(Event event, var detail, Node target) {
     print(translation);
   }
+  
+  void selectedLanguageChanged(oldValue, newValue) {
+    if (oldValue != null) {
+      translationsMap[oldValue] = translation;
+      translations = JSON.encode(translationsMap);
+    }
+    translation = translationsMap[newValue];
+  }
+  
 }
